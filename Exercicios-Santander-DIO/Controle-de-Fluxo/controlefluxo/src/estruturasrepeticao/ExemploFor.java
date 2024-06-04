3b56dda99ca31bf84c2ffd56e8573c2082176003
Luiz Fabio <luizbinho26@gmail.com>
2024-06-04 12:39:01 -0300

    Ad os Exercicios propostos em aula

---
+++ Exercicios-Santander-DIO/Controle-de-Fluxo/controlefluxo/src/estruturasrepeticao/ExemploFor.java
@@ -0,0 +1,12 @@
+package estruturasrepeticao;
+
+public class ExemploFor {
+
+	public static void main(String[] args) {
+		for ( int carneirinhos = 1; carneirinhos <=20; carneirinhos ++) {
+			 System.out.println("Joanzinho contando carneirinhos " + carneirinhos);
+		} 
+		System.out.println("Joanzinho dormiu depois que contou até 20");  
+	}
+
+}
