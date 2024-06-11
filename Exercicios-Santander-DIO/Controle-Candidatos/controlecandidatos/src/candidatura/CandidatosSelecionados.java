1ef537f1385572fb9a616cd60bb3035069aab420
Luiz Fabio <luizbinho26@gmail.com>
2024-06-10 21:06:34 -0300

    Ad os exercícios propostos em aula

---
+++ Exercicios-Santander-DIO/Controle-Candidatos/controlecandidatos/src/candidatura/CandidatosSelecionados.java
@@ -0,0 +1,60 @@
+package candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CandidatosSelecionados {
+
+	public static void main(String[] args) {
+         imprimirSelecionados();
+	}
+    static void imprimirSelecionados() {
+    	String[] candidatos = {"FELIPE", "PAULO", "ROBERTA", "CLARA", "DANIELA"};
+    	
+    	System.out.println("Imprimindo a lista de candidatos informando o indice do elemento ");
+    	
+    	for(int indice =0; indice < candidatos.length; indice++) {
+    		System.out.println("(O/A) candidato(a) de nº" + indice+ " é (o/a): " + candidatos[indice]);	
+    	}
+    	System.out.println("Forma abreviada de interação for each ");
+    	
+    	for(String candidato: candidatos) {
+    		System.out.println("(O/A) candidato(a) selecionado(a) foi: " + candidato);
+    	}
+    }
+
+  static void selecaoCandidatos() {
+    	
+    	String [] candidatos = {"FELIPE", "LUCAS", "ROBERTA", "CLARA", "DANIELA", "PAULO","MARCIA","ANA PAULA","HUGO","PAMELA"};
+    	
+    	int candidatosSelecionados = 0;
+    	int candidatosAtual = 0;
+    	double salarioBase =2000.0;
+    	while (candidatosSelecionados < 5 && candidatosAtual< candidatos.length ) {
+    		//antes de adicionar " && candidatosAtual< candidatos.length " o while apresentava erro. 
+    		
+    		String candidato = candidatos[candidatosAtual];
+    		double salarioPretendido = valorPretendido();
+    		
+    		System.out.println("O candidato: " + candidato + " Solicitou este valor de salário: " + salarioPretendido);
+    		if(salarioBase >= salarioPretendido) {
+    			System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+    			candidatosSelecionados++;
+    		}
+    		candidatosAtual++;
+    	}
+    }	
+    	static double valorPretendido() {
+    			return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    	}		
+    			static void analisarCandidato(double salarioPretendido){
+    		    	double salarioBase = 2000.0;
+    		    	
+    				if(salarioBase > salarioPretendido) {
+    		    		System.out.println("LIGAR PARA O CANDIDATO");		
+    		    	}else if(salarioBase== salarioPretendido )
+    		    		System.out.println("LIGARA PARA O CANDIDATO COM CONTRA PROPOSTA");
+    		    	else {
+    		    		System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+    		    	}
+    		    }
+    		}
---
+++ Exercicios-Santander-DIO/Desafio-java/Contador/desafiocontador/src/desafioJava/Contador.java
@@ -0,0 +1,9 @@
+package desafioJava;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
